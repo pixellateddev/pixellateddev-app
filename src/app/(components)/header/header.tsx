b25df478c805c35e584da8606b87ae3af05f4996
Himanshu Sagar <pixellateddev@proton.me>
2023-12-08 22:43:00 +0530

    added new logo

--- src/app/(components)/header/header.tsx
+++ src/app/(components)/header/header.tsx
@@ -5,7 +5,7 @@ import classes from './header.module.scss'
 const Header: FC = () => {
     return (
        <header className={classes.header}>
-        <Image src='/logo.png' alt="logo"  height={50} width={150}/>
+        <Image src='/logo.svg' alt="logo"  height={50} width={150}/>
        </header>
     )
 
