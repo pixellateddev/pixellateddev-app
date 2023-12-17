6f01293ce609a15bc6c790ed76e3e74f025312c2
Himanshu Sagar <pixellateddev@proton.me>
2023-12-17 23:39:08 +0530

    added antd support

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,19 +1,20 @@
-import prisma from "@/lib/db"
-import { NextPage } from "next"
-import Link from "next/link"
-
+import prisma from '@/lib/db'
+import { Button } from 'antd'
+import { NextPage } from 'next'
+import Link from 'next/link'
 
 const Page: NextPage = async () => {
-    const counters = await prisma.counter.findMany()
-    return (
-       <div>
-        {counters.map(counter => (
-            <div key={counter.id}>
-                <Link href={`/counter/${counter.id}`}>{counter.name}</Link>
-            </div>
-        ))}
-       </div>
-    )
+  const counters = await prisma.counter.findMany()
+  return (
+    <div>
+      {counters.map((counter) => (
+        <div key={counter.id}>
+          <Link href={`/counter/${counter.id}`}>{counter.name}</Link>
+        </div>
+      ))}
+      <Button>Hello</Button>
+    </div>
+  )
 }
 
 export default Page
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Header } from './(components)'
+import AntdProvider from '@/lib/antd'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Header />
-        {children}
+        <AntdProvider>
+          <Header />
+          {children}
+        </AntdProvider>
       </body>
     </html>
   )
